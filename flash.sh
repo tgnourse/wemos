7fe5e4ca072e3e97e06fa44f24c6de03816837bb
Thomas Nourse <tgnourse@thumbtack.com>
2017-02-12 22:18:37 -0800

    Update the default flash version.

--- flash.sh
+++ flash.sh
@@ -1,1 +1,1 @@
-../esptool/esptool.py --port /dev/tty.SLAB_USBtoUART write_flash -fm dio 0x00000 nodemcu-master-9-modules-2016-12-11-07-22-49-float.bin
+../esptool/esptool.py --port /dev/tty.SLAB_USBtoUART write_flash -fm dio 0x00000 nodemcu-master-10-modules-2017-02-12-07-54-28-float.bin
