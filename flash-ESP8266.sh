a80e95b11575fdbcb366e2f40b19acf8cfd0bae4
Thomas Nourse <tgnourse@gmail.com>
2020-09-05 17:44:06 -0700

    Update yellow again

--- flash-ESP8266.sh
+++ flash-ESP8266.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-esptool/esptool.py --port /dev/tty.usbserial-141320 write_flash -fm dio 0x00000 builds/nodemcu-master-10-modules-2018-09-17-22-41-12-float.bin
+python3 esptool/esptool.py --port /dev/tty.usbserial-142320 write_flash -fm dio 0x00000 builds/nodemcu-master-10-modules-2018-09-17-22-41-12-float.bin
